f9be1035d15387856a92936eeadff95db3911c24
StefanYosifov <stefyosifov@gmail.com>
2023-04-15 00:03:37 +0300

    Add login and register

--- [Project]CheatSheet/React/project/src/App.js
+++ [Project]CheatSheet/React/project/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import './styles/style.css'
+import { Navigation } from './components/Navigation/Navigation';
+import { Route, Router, Routes } from 'react-router-dom';
+import LoginPage from './components/Login/Login';
+import RegisterPage from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello react!</h1>
+        <Navigation/>
+        <h1>Hello react!</h1>
+      <Routes>
+        <Route path='/register' Component={RegisterPage} />
+        <Route path='/login' Component={LoginPage} />
+      </Routes>
     </div>
   );
 }
