67a10f46cc878337faa75aff6e66d9be4dd57549
StefanYosifov <stefyosifov@gmail.com>
2023-04-24 22:46:09 +0300

    Testing real time communication with SignalR

---
+++ SignalR Chat/SignalR Chat/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿namespace SignalR_Chat.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using SignalR_Chat.Models;
+    using System.Diagnostics;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SignalR Chat/SignalR Chat/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿namespace SignalR_Chat.Hubs
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.SignalR;
+    using System.Security.Claims;
+
+    public class ChatHub:Hub
+    {
+
+        public async Task SendMessage(string message)
+        {
+            var user = Context.User.Identity.Name;
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    
+
+    }
+}
