be0fe263f71bce0b9f1460c31c24dfc5459ad3cb
StefanYosifov <stefyosifov@gmail.com>
2023-04-28 17:01:12 +0300

    Introduced auto mapper

---
+++ [Project]CheatSheet/[Project]CheatSheet/Common/Mapping/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace _Project_CheatSheet.Common.Mapping
+{
+    using _Project_CheatSheet.Data.Models;
+    using _Project_CheatSheet.Features.Likes.Models;
+    using AutoMapper;
+
+    public class MapperProfile:Profile
+    {
+        public MapperProfile()
+        {
+            this.CreateMap<LikeResourceModel, ResourceLike>()
+                .ForMember(dest => dest.ResourceId, opt => opt.MapFrom(src => Guid.Parse(src.ResourceId)));
+        }
+    }
+}
