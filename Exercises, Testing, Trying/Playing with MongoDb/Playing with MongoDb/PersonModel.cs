2d77a507fc32d8017adb5c76688e8bdea637fbbc
StefanYosifov <stefyosifov@gmail.com>
2023-07-02 23:15:29 +0300

    Some MongoDb testing

---
+++ Exercises, Testing, Trying/Playing with MongoDb/Playing with MongoDb/PersonModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Playing_with_MongoDb
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    internal class PersonModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }= null!;
+
+        [BsonRequired]
+        [BsonRepresentation(BsonType.String)]
+        public string Name { get; set; }= null!;
+
+        [BsonRequired]
+        [BsonRepresentation(BsonType.String)]
+        public string Description { get; set; } = null!;
+
+        [BsonRequired]
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Age { get; set; }
+    }
+}
---
+++ Exercises, Testing, Trying/Playing with MongoDb/Playing with MongoDb/Program.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using Playing_with_MongoDb;
+
+const string connectionString = "mongodb://127.0.0.1:27017";
+const string database = "testing_db";
+const string collectionName = "people";
+
+
+var client = new MongoClient(connectionString);
+var db = client.GetDatabase(collectionName);
+var collection = db.GetCollection<PersonModel>(collectionName);
+
+
+var person = new PersonModel
+{
+    Name = "Pesho2",
+    Description = "This is a quick description for pesho2",
+    Age = 22
+};
+
+if (await collection.FindAsync(x => x.Name == person.Name) == null)
+{
+    await collection.InsertOneAsync(person);
+}
+
+var results = await collection.FindAsync(x => true);
+
+await results.ForEachAsync(x => Console.WriteLine($"{x.Name} {x.Age} {x.Description}"));
